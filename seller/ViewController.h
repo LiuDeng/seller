dba93195a10b631862e5137404e45ce26a746914
余文华 <18668089860@163.com>
2015-07-08 19:02:20 +0800

    first

---
+++ seller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  seller
+//
+//  Created by 余文华 on 15/7/8.
+//  Copyright (c) 2015年 ds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ seller/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  seller
+//
+//  Created by 余文华 on 15/7/8.
+//  Copyright (c) 2015年 ds. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
